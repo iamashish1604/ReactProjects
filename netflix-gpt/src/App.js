417b91f112cd8994f7beb9a9e9a56debfc4359e7
Ashish Singh <iamashish1604@gmail.com>
2025-08-16 19:15:00 +0530

    intial project setup and tailwind added

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='font-bold text-3xl'>hello world</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
